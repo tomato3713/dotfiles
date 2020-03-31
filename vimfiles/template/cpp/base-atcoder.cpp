cffe9edb1f9d359266673e19c292b1846b769a0b
tomato3713 <weasel.wt@gmail.com>
2020-03-31 23:54:27 +0900

    AtCoder 用に更新

---
+++ vimfiles/template/cpp/base-atcoder.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+// data structure
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <vector>
+
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  string in;
+  cin >> in;
+  return 0;
+}
