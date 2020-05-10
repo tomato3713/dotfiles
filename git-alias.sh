72c62a2cf28b1b57e5c35b4f94b01c9e8b83132f
tomato3713 <weasel.wt@gmail.com>
2020-05-11 07:56:33 +0900

    Update git config script

--- git-alias.sh
+++ git-alias.sh
@@ -1,7 +1,14 @@
 git config --global alias.s status
 git config --global alias.d diff
 git config --global alias.b branch
-git config --global alias.l log
+git config --global alias.l 'log --oneline'
 git config --global alias.g 'log --graph'
 git config --global alias.a add
 git config --global alias.ignore '!gi() { curl -L -s https://www.gitignore.io/api/$@ ;}; gi'
+
+# vimdiff
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global merge.tool vimdiff
+git config --global mergetool.prompt false
+git config --global merge.conflictstyle diff3
