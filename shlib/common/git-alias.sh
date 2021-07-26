3e3f39367a4799051ba42638a07427755e48ccd6
taichi watanabe <tomato3713@outlook.com>
2021-07-26 09:45:05 +0900

    add git alias: co as checkout

--- shlib/common/git-alias.sh
+++ shlib/common/git-alias.sh
@@ -3,6 +3,7 @@ git config --global alias.d diff
 git config --global alias.b branch
 git config --global alias.l 'log --oneline --graph'
 git config --global alias.a add
+git config --global alias.co checkout
 git config --global alias.ignore '!gi() { curl -L -s https://www.gitignore.io/api/$@ ;}; gi'
 
 # vimdiff
