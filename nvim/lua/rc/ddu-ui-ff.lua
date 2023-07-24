eaee2526cc86b2345e6ee5c4306e7b9f57e77366
tomato3713 <tomato3713@outlook.com>
2023-07-24 23:12:27 +0900

    nvim: ddu-ui-ff

---
+++ nvim/lua/rc/ddu-ui-ff.lua
@@ -0,0 +1,13 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = 'ddu-ff-filter',
+	callback = function()
+		local opts = { silent = true }
+		vim.keymap.set('i', '<CR>', '<Esc><Cmd>call ddu#ui#ff#do_action("itemAction")<CR>', opts)
+		vim.keymap.set('i', '<Esc>', '<Esc><Cmd>call ddu#ui#ff#do_action("quit")<CR>', opts)
+		vim.keymap.set('i', '<C-n>', '<C-o><Cmd>call ddu#ui#ff#execute(\'normal j\')<CR>', opts)
+		vim.keymap.set('i', '<C-p>', '<C-o><Cmd>call ddu#ui#ff#execute(\'normal k\')<CR>', opts)
+		vim.keymap.set('i', '<C-t>', '<C-o><Cmd>call ddu#ui#ff#do_action("preview")<CR>', opts)
+		vim.keymap.set('i', '<C-e>',
+			'<C-o><Cmd>call ddu#ui#ff#do_action("expandItem", { "mode":    "toggle" })<CR>', opts)
+	end
+})
