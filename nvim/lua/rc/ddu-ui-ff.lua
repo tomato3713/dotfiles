09270d774e47481d48fb30a9f0ebacdb8bb9e710
tomato3713 <tomato3713@outlook.com>
2023-07-26 21:28:15 +0900

    fix

--- nvim/lua/rc/ddu-ui-ff.lua
+++ nvim/lua/rc/ddu-ui-ff.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = 'ddu-ff-filter',
 	callback = function()
-		local opts = { silent = true }
+		local opts = { silent = true, buffer = true }
 		vim.keymap.set('i', '<CR>', '<Esc><Cmd>call ddu#ui#ff#do_action("itemAction")<CR>', opts)
 		vim.keymap.set('i', '<Esc>', '<Esc><Cmd>call ddu#ui#ff#do_action("quit")<CR>', opts)
 		vim.keymap.set('i', '<C-n>', '<C-o><Cmd>call ddu#ui#ff#execute(\'normal j\')<CR>', opts)
