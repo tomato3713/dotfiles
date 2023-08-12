44ee79770a6fd2ebc206b3b69212932c48de2546
tomato3713 <tomato3713@outlook.com>
2023-08-12 21:33:50 +0900

    nvim: fix keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,7 +58,7 @@ end
 api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "help", "lspinfo", "qf" },
 	callback = function()
-		vim.keymap.set('n', 'q', '<Cmd>close<CR>')
+		vim.keymap.set('n', 'q', function() vim.api.nvim_win_close(0, true) end, { buffer = true })
 	end,
 })
 
