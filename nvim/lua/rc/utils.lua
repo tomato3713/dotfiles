5fedd261541095d0999d11fb9a15b5940b12a8c7
tomato3713 <tomato3713@outlook.com>
2023-10-18 10:10:15 +0900

    create lua dump function

--- nvim/lua/rc/utils.lua
+++ nvim/lua/rc/utils.lua
@@ -3,9 +3,13 @@ local M = {}
 M.try_catch = function(what)
 	local status, result = pcall(what.try)
 	if not status then
-		if what.catch ~= nil then what.catch(result) end
+		if what.catch ~= nil then
+			what.catch(result)
+		end
 	end
-	if what.finally ~= nil then what.finally(result) end
+	if what.finally ~= nil then
+		what.finally(result)
+	end
 	return result
 end
 
@@ -21,7 +25,7 @@ M.keep_cursor = function(cmd)
 				if vim.fn.win_getid() == curwin_id then
 					vim.fn.winrestview(view)
 				end
-			end
+			end,
 		})
 	end
 end
@@ -35,6 +39,27 @@ M.contains = function(list, element)
 		end
 	end
 	return false
+end
+
+-- @params obj object
+-- @params indent number
+-- @return string
+local function dumper(o, indent)
+	if type(o) == "table" then
+		local s = "{\n"
+		for k, v in pairs(o) do
+			s = s .. string.rep(" ", indent) .. "'" .. k .. "' = " .. dumper(v, indent + 1) .. ",\n"
+		end
+		return s .. string.rep(" ", indent) .. "}"
+	else
+		return tostring(o)
+	end
+end
+
+-- @params obj object
+-- @return string
+M.dump = function(o)
+	return dumper(o, 0)
 end
 
 return M
