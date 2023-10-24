8c78f30286acfa35b57d33635c8499721eafad01
tomato3713 <tomato3713@outlook.com>
2023-10-25 00:12:00 +0900

    update lua format rules

--- nvim/lua/rc/formatter.lua
+++ nvim/lua/rc/formatter.lua
@@ -1,70 +1,43 @@
--- Utilities for creating configurations
-local util = require("formatter.util")
-
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup({
+require('formatter').setup({
 	-- Enable or disable logging
 	logging = false,
 	-- Set the log level
 	log_level = vim.log.levels.WARN,
 	-- All formatter configurations are opt-in
 	filetype = {
-		-- Formatter configurations for filetype "lua" go here
-		-- and will be executed in order
 		lua = {
-			-- "formatter.filetypes.lua" defines default configurations for the
-			-- "lua" filetype
-			require("formatter.filetypes.lua").stylua,
-
-			-- You can also define your own configuration
 			function()
-				-- Supports conditional formatting
-				if util.get_current_buffer_file_name() == "special.lua" then
-					return nil
-				end
-
-				-- Full specification of configurations is down below and in Vim help
-				-- files
-				return {
-					exe = "stylua",
-					args = {
-						"--search-parent-directories",
-						"--stdin-filepath",
-						util.escape_path(util.get_current_buffer_file_path()),
-						"--",
-						"-",
-					},
-					stdin = true,
-				}
+				vim.lsp.buf.format({ async = false })
 			end,
 		},
 
 		typescriptreact = {
-			require("formatter.filetypes.typescriptreact").prettier,
+			require('formatter.filetypes.typescriptreact').prettier,
 		},
 
 		typescript = {
-			require("formatter.filetypes.typescript").prettier,
+			require('formatter.filetypes.typescript').prettier,
 		},
 
 		javascript = {
-			require("formatter.filetypes.javascript").prettier,
+			require('formatter.filetypes.javascript').prettier,
 		},
 
 		css = {
-			require("formatter.filetypes.css").prettier,
+			require('formatter.filetypes.css').prettier,
 		},
 
 		go = {
-			require("formatter.filetypes.go").gofmt,
+			require('formatter.filetypes.go').gofmt,
 		},
 
 		sql = {
 			function()
 				return {
-					exe = "sql-formatter",
+					exe = 'sql-formatter',
 					args = {
-						"--fix",
+						'--fix',
 					},
 				}
 			end,
@@ -73,10 +46,10 @@ require("formatter").setup({
 		perl = {
 			function()
 				return {
-					exe = "perltidy",
+					exe = 'perltidy',
 					args = {
 						-- ファイルに出力せず、標準出力に出力する
-						"-st",
+						'-st',
 					},
 					stdin = true,
 				}
@@ -85,10 +58,10 @@ require("formatter").setup({
 
 		-- Use the special "*" filetype for defining formatter configurations on
 		-- any filetype
-		["*"] = {
+		['*'] = {
 			-- "formatter.filetypes.any" defines default configurations for any
 			-- filetype
-			require("formatter.filetypes.any").remove_trailing_whitespace,
+			require('formatter.filetypes.any').remove_trailing_whitespace,
 		},
 	},
 })
