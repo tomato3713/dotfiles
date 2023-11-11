8ec8e5b301b8e1b8e731e7c167370cae8e65b4a7
tomato3713 <tomato3713@outlook.com>
2023-11-11 23:13:41 +0900

    fix mr.vim configuration

--- nvim/lua/rc/mr.lua
+++ nvim/lua/rc/mr.lua
@@ -1,10 +1,15 @@
 -- do not record mr list
 local no_record_list = {
+	---@param filename string
+	---@return boolean
 	function(filename)
-		return string.match(filename, "COMMIT_EDITMSG") ~= nil
+		return not (string.find(filename, "doc/.*%.txt$")
+			or string.find(filename,"doc/.*%.jax$")
+			or string.find(filename, "/.git/COMMIT_EDITMSG")
+		)
 	end,
 }
 
-vim.api.nvim_set_var("mr#mru#predicates", no_record_list)
-vim.api.nvim_set_var("mr#mrw#predicates", no_record_list)
-vim.api.nvim_set_var("mr#mrr#predicates", no_record_list)
+vim.g["mr#mru#predicates"] = no_record_list
+vim.g["mr#mrw#predicates"] = no_record_list
+vim.g["mr#mrr#predicates"] = no_record_list
