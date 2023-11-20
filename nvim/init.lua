05ebc926597e7fcb7839c5a8ac27271465a61f3b
tomato3713 <tomato3713@outlook.com>
2023-11-20 19:48:32 +0900

    cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,47 +1,37 @@
-local utils = require('rc.utils')
-local o = vim.o
-local opt = vim.opt
-local fn = vim.fn
-local api = vim.api
+local my_gruop = vim.api.nvim_create_augroup('vimrcEx', { clear = true })
 
 -- encoding setting
-o.encoding = 'utf-8'                 -- バッファ内で扱う文字コード
-o.fileencoding = 'utf-8'             -- 書き込み時UTF-8出力
-o.fileencodings = 'utf-8,cp932,sjis' -- 読み込み時UTF-8, CP932, Shift_JISの順で自動判別
-o.fileformats = 'unix,dos,mac'
-o.completeslash = 'slash'
+vim.o.encoding = 'utf-8'                             -- バッファ内で扱う文字コード
+vim.o.fileencoding = 'utf-8'                         -- 書き込み時UTF-8出力
+vim.opt.fileencodings = { 'utf-8', 'cp932', 'sjis' } -- 読み込み時UTF-8, CP932, Shift_JISの順で自動判別
+vim.opt.fileformats = { 'unix', 'dos', 'mac' }
 
 -- view
-o.pumblend = 10
-o.termguicolors = true
-o.showmode = false
-o.signcolumn = 'yes'
-o.laststatus = 3 -- v0.7.0 and later
+vim.o.pumblend = 10
+vim.o.termguicolors = true
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+vim.o.laststatus = 3 -- v0.7.0 and later
+vim.opt.cursorline = true
+vim.o.list = true
+vim.opt.listchars = { tab = '>>', trail = '_', nbsp = '+' }
 
 -- editor setting
-o.imd = false        -- IM disable
-o.sb = true          -- 水平分割時に下に表示
-o.sr = false         -- 縦分割時を右に表示
-o.smartindent = true -- スマートインデントを行う
-o.list = true
-opt.listchars = { tab = '>>', trail = '_', nbsp = '+' }
-vim.opt.wrapscan = false
+vim.o.smartindent = true -- スマートインデントを行う
 
 -- temporary file setting
-opt.undofile = true
-vim.opt.undodir = fn.stdpath('cache') .. '/undo'
-opt.swapfile = false -- swapfileを作らない
-opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath('cache') .. '/undo'
+vim.opt.swapfile = false
+vim.opt.backup = false
 
-opt.cursorline = true
-opt.ruler = false
-
--- tab setting
-o.ignorecase = true
+-- search
+vim.o.ignorecase = true
+vim.opt.wrapscan = false -- no loop when search word
 
 -- set wildmenu wildmode=longest:full
-o.wildmenu = false
-opt.wildignore = {
+vim.o.wildmenu = false
+vim.opt.wildignore = {
 	'*.o',
 	'*.obj',
 	'*.class',
@@ -56,62 +46,62 @@ opt.wildignore = {
 	'*.pptx',
 	'*.docs',
 }
-o.timeoutlen = 500
 
 -- terminal mode
 local sysname = vim.loop.os_uname().sysname
 if sysname == 'win32' or sysname == 'win64' then
-	o.shell = 'nyagos'
-	o.shcf = '-c'
-	fn.shellxquote('')
-	fn.shellxescape('')
-else
-	o.shell = 'zsh'
+	vim.o.shell = 'nyagos'
+	vim.o.shcf = '-c'
+	vim.fn.shellxquote('')
+	vim.fn.shellxescape('')
+elseif sysname == 'Darwin' then
+	vim.o.shell = 'zsh'
 end
 
 --- close by 'q'
-api.nvim_create_autocmd({ 'FileType' }, {
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+	group = my_gruop,
 	pattern = { 'help', 'lspinfo', 'qf' },
 	callback = function()
 		vim.keymap.set('n', 'q', function()
 			vim.api.nvim_win_close(0, true)
-		end, { buffer = true })
+		end, { buffer = true, noremap = true })
 	end,
 })
 
 -- set default filtype as plain text
 function NoneFileTypeSetTxt()
-	if string.len(o.filetype) == 0 then
-		o.filetype = 'txt'
+	if string.len(vim.o.filetype) == 0 then
+		vim.o.filetype = 'txt'
 	end
 end
 
-api.nvim_create_autocmd({ 'BufEnter' }, {
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+	group = my_gruop,
 	callback = NoneFileTypeSetTxt,
 })
 
 -- toggle "，/．" and "、/。"
-api.nvim_create_user_command('CommaPeriod', function(opts)
-	local cursor = fn.getcurpos()
+vim.api.nvim_create_user_command('CommaPeriod', function(opts)
+	local cursor = vim.fn.getcurpos()
 	vim.api.nvim_command('silent keepjumps keeppatterns' .. opts.line1 .. ',' .. opts.line2 .. 's/、/，/ge')
 	vim.api.nvim_command('silent keepjumps keeppatterns' .. opts.line1 .. ',' .. opts.line2 .. 's/。/．/ge')
-	fn.setpos('.', cursor)
+	vim.fn.setpos('.', cursor)
 end, { range = true })
 
-api.nvim_create_user_command('Kutoten', function(opts)
-	local cursor = fn.getcurpos()
+vim.api.nvim_create_user_command('Kutoten', function(opts)
+	local cursor = vim.fn.getcurpos()
 	vim.api.nvim_command('silent keepjumps keeppatterns' .. opts.line1 .. ',' .. opts.line2 .. 's/，/、/ge')
 	vim.api.nvim_command('silent keepjumps keeppatterns' .. opts.line1 .. ',' .. opts.line2 .. 's/．/。/ge')
-	fn.setpos('.', cursor)
+	vim.fn.setpos('.', cursor)
 end, { range = true })
 
-api.nvim_create_user_command('CountChars', function(opts)
-	local cursor = fn.getcurpos()
+vim.api.nvim_create_user_command('CountChars', function(opts)
+	local cursor = vim.fn.getcurpos()
 	vim.api.nvim_command('keepjumps keeppatterns' .. opts.line1 .. ',' .. opts.line2 .. 's/./&/gn')
-	fn.setpos('.', cursor)
+	vim.fn.setpos('.', cursor)
 end, { range = true })
 
-local my_gruop = vim.api.nvim_create_augroup('vimrcEx', { clear = true })
 -- http://advweb.seesaa.net/article/13443981.html
 -- jump to the last line the cursor was on.
 vim.api.nvim_create_autocmd({ 'BufRead' }, {
@@ -125,17 +115,17 @@ vim.api.nvim_create_autocmd({ 'BufRead' }, {
 })
 
 -- dein Scripts
-local dein_dir = fn.expand('~/.cache/dein')
+local dein_dir = vim.fn.expand('~/.cache/dein')
 local dein_repo_dir = dein_dir .. '/repos/github.com/Shougo/dein.vim'
 
 -- https://github.com/settings/tokens
-api.nvim_set_var('dein#install_github_api_token', os.getenv('DEIN_GITHUB_TOKEN'))
+vim.api.nvim_set_var('dein#install_github_api_token', os.getenv('DEIN_GITHUB_TOKEN'))
 
-if not string.find(api.nvim_get_option('runtimepath'), '/dein.vim') then
-	if not (fn.isdirectory(dein_repo_dir) == 1) then
+if not string.find(vim.api.nvim_get_option('runtimepath'), '/dein.vim') then
+	if not (vim.fn.isdirectory(dein_repo_dir) == 1) then
 		os.execute('git clone https://github.com/Shougo/dein.vim ' .. dein_repo_dir)
 	end
-	api.nvim_set_option('runtimepath', dein_repo_dir .. ',' .. api.nvim_get_option('runtimepath'))
+	vim.api.nvim_set_option('runtimepath', dein_repo_dir .. ',' .. vim.api.nvim_get_option('runtimepath'))
 end
 
 local dein = require('dein')
@@ -146,7 +136,7 @@ dein.setup({
 })
 
 dein.begin(dein_dir)
-dein.load_toml(fn.expand('~/.config/nvim/dein.toml'), { lazy = 0 })
+dein.load_toml(vim.fn.expand('~/.config/nvim/dein.toml'), { lazy = 0 })
 dein.end_()
 dein.save_state()
 
@@ -160,6 +150,6 @@ if vim.fn.len(removed_plugins) > 0 then
 	dein.recache_runtimepath()
 end
 
-api.nvim_create_user_command('DeinClearCache', function()
+vim.api.nvim_create_user_command('DeinClearCache', function()
 	dein.recache_runtimepath()
 end, {})
