08492e8e0650636cf7388ac91aa8afc7cb7000f0
tomato3713 <tomato3713@outlook.com>
2024-01-17 21:22:07 +0900

    skkeleton config update

--- nvim/lua/rc/skkeleton.lua
+++ nvim/lua/rc/skkeleton.lua
@@ -2,7 +2,6 @@ vim.keymap.set({ 'i', 'c', 't' }, '<C-j>', '<Plug>(skkeleton-toggle)')
 
 local skk_dir = '~/.config/skk'
 local global_skk_jisyo = vim.fn.expand(skk_dir .. '/SKK-JISYO.L')
-local user_skk_jisyo = vim.fn.expand(skk_dir .. '/user-dict')
 
 -- download skk dictionary file
 local download_global_skk_dictionary = function(global_dictionary)
@@ -10,28 +9,20 @@ local download_global_skk_dictionary = function(global_dictionary)
 	os.execute('curl ' .. skk_dict_url .. ' -o ' .. vim.fn.expand(global_dictionary))
 end
 
-local init_skk_dictionary = function(dir, global_dictionary, user_dictionary)
+local init_skk_dictionary = function(dir, global_dictionary)
 	if not (vim.fn.isdirectory(dir) == 0) then
 		vim.fn.mkdir(dir)
 
 		download_global_skk_dictionary(global_dictionary)
-		vim.fn.writefile({}, user_dictionary)
 	end
 end
 
-vim.api.nvim_create_user_command('DownloadSKKDict', function()
-	download_global_skk_dictionary(global_skk_jisyo)
-end, {})
-
-init_skk_dictionary(skk_dir, global_skk_jisyo, user_skk_jisyo)
-
-vim.fn["denops#plugin#wait_async"]("skkeleton", function()
+local init = function()
 	vim.fn['skkeleton#config']({
 		eggLikeNewline = true,
 		keepState = false,
-		globalJisyo = global_skk_jisyo,
-		userJisyo = user_skk_jisyo,
-		immediatelyJisyoRW = false,
+		globalDictionaries = { { global_skk_jisyo, 'euc-jp' } },
+		immediatelyDictionaryRW = false,
 		markerHenkan = '|',
 		markerHenkanSelect = '?'
 	})
@@ -41,6 +32,18 @@ vim.fn["denops#plugin#wait_async"]("skkeleton", function()
 		["z,"] = { "，", '' },
 		["z<Space>"] = { "　", '' },
 	})
+end
 
-	vim.fn['skkeleton#initialize']()
-end)
+vim.api.nvim_create_user_command('DownloadSKKDict', function()
+	download_global_skk_dictionary(global_skk_jisyo)
+end, {})
+
+require('rc.utils').nvim_create_autocmd('User', {
+	group = 'skkeleton-initialize-pre',
+	callback = init,
+	desc = 'init skkeleton',
+})
+
+init_skk_dictionary(skk_dir, global_skk_jisyo)
+
+init()
