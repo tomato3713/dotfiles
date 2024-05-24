a661241282f543c9e286a9f76080a2a870fbe4a5
tomato3713 <tomato3713@outlook.com>
2024-05-24 20:46:44 +0900

    dial.nvim

---
+++ nvim/lua/rc/dial.lua
@@ -0,0 +1,50 @@
+local augend = require("dial.augend")
+require("dial.config").augends:register_group {
+	default = {
+		augend.integer.alias.decimal,
+		augend.integer.alias.hex,
+		augend.date.alias["%Y/%m/%d"],
+	},
+	typescript = {
+		augend.integer.alias.decimal,
+		augend.integer.alias.hex,
+		augend.constant.new { elements = { "true", "false" } },
+		augend.constant.new { elements = { "let", "const" } },
+	},
+	lua = {
+		augend.integer.alias.decimal,
+		augend.integer.alias.hex,
+		augend.constant.new { elements = { "true", "false" } },
+	},
+	visual = {
+		augend.integer.alias.decimal,
+		augend.integer.alias.hex,
+		augend.date.alias["%Y/%m/%d"],
+		augend.constant.alias.alpha,
+		augend.constant.alias.Alpha,
+	},
+}
+
+-- change augends in VISUAL mode
+vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual("visual"), { noremap = true })
+vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual("visual"), { noremap = true })
+
+require('rc.utils').nvim_create_autocmd({ 'FileType' }, {
+	pattern = 'typescript',
+	callback = function()
+		vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal("typescript"),
+			{ noremap = true, buffer = true })
+		vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal("typescript"),
+			{ noremap = true, buffer = true })
+	end
+})
+
+require('rc.utils').nvim_create_autocmd({ 'FileType' }, {
+	pattern = 'lua',
+	callback = function()
+		vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal("lua"),
+			{ noremap = true, buffer = true })
+		vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal("lua"),
+			{ noremap = true, buffer = true })
+	end
+})
