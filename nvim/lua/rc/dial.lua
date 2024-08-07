aeb91d9c234c2f4645c017270bbffa6bf3c840e3
tomato3713 <tomato3713@outlook.com>
2024-08-08 00:56:46 +0900

    replace dial.nvim to dps-dial.vim

--- nvim/lua/rc/dial.lua
+++ nvim/lua/rc/dial.lua
@@ -1,66 +1,17 @@
-local augend = require("dial.augend")
-require("dial.config").augends:register_group {
-	default = {
-		augend.integer.alias.decimal,
-		augend.integer.alias.hex,
-		augend.date.alias["%Y/%m/%d"],
-	},
-	go = {
-		augend.integer.alias.decimal,
-		augend.integer.alias.hex,
-		augend.constant.new { elements = { "true", "false" } },
-		augend.constant.new { elements = { "var", "const" } },
-	},
-	typescript = {
-		augend.integer.alias.decimal,
-		augend.integer.alias.hex,
-		augend.constant.new { elements = { "true", "false" } },
-		augend.constant.new { elements = { "let", "const" } },
-	},
-	lua = {
-		augend.integer.alias.decimal,
-		augend.integer.alias.hex,
-		augend.constant.new { elements = { "true", "false" } },
-	},
-	visual = {
-		augend.integer.alias.decimal,
-		augend.integer.alias.hex,
-		augend.date.alias["%Y/%m/%d"],
-		augend.constant.alias.alpha,
-		augend.constant.alias.Alpha,
-	},
+vim.g["dps_dial#augends"] = {
+	'decimal',
+	'date-slash',
+	{ kind = 'constant', opts = { elements = { 'true', 'false' }, cyclic = true, word = false } },
+	{ kind = 'constant', opts = { elements = { 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' }, cycle = true, word = true } },
+	{ kind = 'constant', opts = { elements = { 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday' }, cycle = true, word = true } },
+	{ kind = 'case',     opts = { cases = { 'camelCase', 'snake_case' }, cyclic = true } },
 }
-
--- change augends in VISUAL mode
-vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual("visual"), { noremap = true })
-vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual("visual"), { noremap = true })
 
 require('my.utils').nvim_create_autocmd({ 'FileType' }, {
 	pattern = 'typescript',
 	callback = function()
-		vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal("typescript"),
-			{ noremap = true, buffer = true })
-		vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal("typescript"),
-			{ noremap = true, buffer = true })
-	end
-})
-
-require('my.utils').nvim_create_autocmd({ 'FileType' }, {
-	pattern = 'lua',
-	callback = function()
-		vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal("lua"),
-			{ noremap = true, buffer = true })
-		vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal("lua"),
-			{ noremap = true, buffer = true })
-	end
-})
-
-require('my.utils').nvim_create_autocmd({ 'FileType' }, {
-	pattern = 'go',
-	callback = function()
-		vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal("go"),
-			{ noremap = true, buffer = true })
-		vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal("go"),
-			{ noremap = true, buffer = true })
+		vim.b["dps_dial#augends"] = {
+			{ kind = 'constant', opts = { elements = { 'let', 'const' }, cyclic = true, word = true } },
+		}
 	end
 })
