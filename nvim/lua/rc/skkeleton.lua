8fd265433d9e57f7050c4486c1700ec0d8f0485f
tomato3713 <tomato3713@outlook.com>
2024-08-07 23:49:41 +0900

    user dictionary

--- nvim/lua/rc/skkeleton.lua
+++ nvim/lua/rc/skkeleton.lua
@@ -1,5 +1,6 @@
 local skk_dir = '~/.config/skk'
 local global_skk_jisyo = vim.fn.expand(skk_dir .. '/SKK-JISYO.L')
+local user_skk_jisyo = vim.fn.expand(skk_dir .. '/user.dict')
 
 local init_skk_dictionary = function(global_dictionary_path, is_override)
 	local dictionary_url = 'http://openlab.jp/skk/skk/dic/SKK-JISYO.L'
@@ -10,8 +11,11 @@ local init = function()
 	vim.fn['skkeleton#config']({
 		eggLikeNewline = true,
 		keepState = false,
-		globalDictionaries = { { global_skk_jisyo, 'euc-jp' } },
-		immediatelyDictionaryRW = false,
+		globalDictionaries = {
+			{ global_skk_jisyo, 'euc-jp' },
+		},
+		userDictionary = user_skk_jisyo,
+		immediatelyDictionaryRW = true,
 		markerHenkan = '|',
 		markerHenkanSelect = '?'
 	})
@@ -38,7 +42,7 @@ require('my.utils').nvim_create_autocmd("User", {
 })
 
 vim.api.nvim_create_user_command('DownloadSKKDict', function()
-init_skk_dictionary(global_skk_jisyo, true)
+	init_skk_dictionary(global_skk_jisyo, true)
 end, {})
 
 require('my.utils').nvim_create_autocmd('User', {
