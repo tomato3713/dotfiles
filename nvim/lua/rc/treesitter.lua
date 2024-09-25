64516e97290b9c47b3fa2d573427d76e1bb31a4e
tomato3713 <tomato3713@outlook.com>
2024-09-25 21:55:31 +0900

    define new textobject with treesitter

--- nvim/lua/rc/treesitter.lua
+++ nvim/lua/rc/treesitter.lua
@@ -19,4 +19,12 @@ require 'nvim-treesitter.configs'.setup {
 	context_commentstring = {
 		enable_autocmd = false,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<CR>",
+			node_incremental = "<CR>",
+			node_decremental = "<S-CR>",
+		},
+	},
 }
