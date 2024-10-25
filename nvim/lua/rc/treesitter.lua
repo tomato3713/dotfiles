3898861c674502a3db04b914d6399e107b33676d
tomato3713 <tomato3713@outlook.com>
2024-10-25 19:20:13 +0900

    go template syntax highlight

--- nvim/lua/rc/treesitter.lua
+++ nvim/lua/rc/treesitter.lua
@@ -1,6 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
 	ensure_installed = {
 		"go",
+		"gotmpl",
 		"tsx", "css",
 		"lua",
 		"perl",
@@ -28,3 +29,32 @@ require 'nvim-treesitter.configs'.setup {
 		},
 	},
 }
+
+-- go template engine
+-- local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+-- parser_config.gotmpl = {
+-- 	install_info = {
+-- 		url = "https://github.com/ngalaiko/tree-sitter-go-template",
+-- 		files = { "src/parser.c" }
+-- 	},
+-- 	filetype = "gotmpl",
+-- 	used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" }
+-- }
+
+vim.filetype.add({
+	extension = {
+		gotmpl = 'gotmpl',
+	},
+	pattern = {
+		[".*/templates/.*%.tpl"] = "helm",
+		[".*/templates/.*%.ya?ml"] = "helm",
+		["helmfile.*%.ya?ml"] = "helm",
+	},
+})
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = { "*.tmpl" },
+	callback = function()
+			vim.bo.filetype = "gotmpl"
+	end,
+})
