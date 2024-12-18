d358b8a751289f0d026d4bf4c485f2eca3311984
tomato3713 <tomato3713@outlook.com>
2024-12-19 00:35:58 +0900

    update pum.vim configuration

--- nvim/lua/rc/pum.lua
+++ nvim/lua/rc/pum.lua
@@ -2,6 +2,8 @@ vim.fn['pum#set_option']({
 	item_orders = { 'kind', 'space', 'abbr', 'space', 'menu' },
 	max_width = 80,
 	max_height = 10,
-	border = 'rounded',
+	border = 'single',
 	offset_cmdrow = 3,
+	padding = true,
+	preview = true,
 })
