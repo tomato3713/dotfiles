07ab62f0558417def5a112034366f2dd3441c6f5
tomato3713 <tomato3713@outlook.com>
2024-12-27 00:19:08 +0900

    append other.nvim mapping

--- nvim/lua/rc/other-nvim.lua
+++ nvim/lua/rc/other-nvim.lua
@@ -1,34 +1,36 @@
 require("other-nvim").setup({
-    mappings = {
-        -- builtin mappings
-        "golang",
-        -- custom mapping
-        {
-            pattern = "/path/to/file/src/app/(.*)/.*.ext$",
-            target = "/path/to/file/src/view/%1/",
-            transformer = "lowercase"
-        }
-    },
-    transformers = {
-        -- defining a custom transformer
-        lowercase = function (inputString)
-            return inputString:lower()
-        end
-    },
-    style = {
-        -- How the plugin paints its window borders
-        -- Allowed values are none, single, double, rounded, solid and shadow
-        border = "solid",
+	mappings = {
+		-- builtin mappings
+		"golang",
+		"react",
+		-- open storybook
+		{
+			pattern = "/components/(.*)/(.*).tsx$",
+			target = "/components/%1/%2.stories.tsx",
+			context = "storybook"
+		}
+	},
+	transformers = {
+		-- defining a custom transformer
+		lowercase = function(inputString)
+			return inputString:lower()
+		end
+	},
+	style = {
+		-- How the plugin paints its window borders
+		-- Allowed values are none, single, double, rounded, solid and shadow
+		border = "solid",
 
-        -- Column seperator for the window
-        seperator = "|",
+		-- Column separator for the window
+		separator = "|",
 
-	-- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
-	width = 0.7,
+		-- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
+		width = 0.7,
 
-	-- min height in rows.
-	-- when more columns are needed this value is extended automatically
-	minHeight = 2
-    },
+		-- min height in rows.
+		-- when more columns are needed this value is extended automatically
+		minHeight = 2
+	},
 })
 
+vim.api.nvim_set_keymap("n", "go", "<cmd>:Other<CR>", { noremap = true, silent = true })
