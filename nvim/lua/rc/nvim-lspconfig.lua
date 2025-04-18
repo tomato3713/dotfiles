80cbcb4c07b388fcad9271c4b7ecd22f465d7be6
tomato3713 <tomato3713@outlook.com>
2025-04-18 19:21:16 +0900

    nvim-cmp

--- nvim/lua/rc/nvim-lspconfig.lua
+++ nvim/lua/rc/nvim-lspconfig.lua
@@ -147,6 +147,8 @@ mason_lspconfig.setup_handlers({
 		end
 
 		opts.on_attach = on_attach
+		-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		-- opts.capabilities = capabilities
 
 		nvim_lsp[server_name].setup(opts)
 	end,
