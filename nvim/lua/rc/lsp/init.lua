7536a7a78e4a3a1b8cf19ccb995a06ff119b2d76
tomato3713 <tomato3713@outlook.com>
2025-05-09 01:25:17 +0900

    deprecated

--- nvim/lua/rc/lsp/init.lua
+++ nvim/lua/rc/lsp/init.lua
@@ -3,7 +3,13 @@ local register_servers = require('rc.lsp.register_servers')
 
 -- setup mason
 require('mason').setup({
-	ui = { icons = { package_installed = "＊", package_pending = "∴", package_uninstalled = "×" } }
+	ui = {
+		icons = {
+			package_installed = "＊",
+			package_pending = "∴",
+			package_uninstalled = "×"
+		}
+	}
 })
 
 require("mason-lspconfig").setup {
@@ -18,18 +24,18 @@ vim.lsp.set_log_level("OFF")
 
 -- LSPの警告フォーマット
 -- ref: https://dev.classmethod.jp/articles/eetann-change-neovim-lsp-diagnostics-format/
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.buf(vim.lsp.diagnostic.on_publish_diagnostics, {
 	virtual_text = {
 		format = function(diagnostic)
 			return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
 		end,
 	},
 })
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf(vim.lsp.buf.hover, {
 	silent = true,
 	border = "single",
 })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf(vim.lsp.buf.signature_help, {
 	silent = true,
 	border = "single",
 })
