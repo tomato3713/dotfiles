915d4d7ade238844f5da280c40efa14c0337cec5
tomato3713 <tomato3713@outlook.com>
2025-06-26 22:40:41 +0900

    macro editor

--- nvim/lua/my/fn.lua
+++ nvim/lua/my/fn.lua
@@ -40,5 +40,26 @@ _M.open_browser_or_buffer_under_cursor = function()
 	end
 end
 
+_M.macro_editor = function()
+	local reg = string.char(vim.fn.getchar())
+
+	local reg_content = vim.fn.getreg(reg)
+
+	if reg_content == "" then
+		vim.notify(string.format("Macro '%s' is empty", reg))
+		return
+	end
+
+	vim.ui.input({
+		prompt = "Edit a macro",
+		default = reg_content,
+	}, function(i)
+		if i == nil or i == "" then
+			vim.notify(string.format("Edit a macro '%s' canceled", reg))
+			return
+		end
+		vim.fn.setreg(reg, i)
+	end)
+end
 
 return _M
--- nvim/lua/my/keymap.lua
+++ nvim/lua/my/keymap.lua
@@ -3,10 +3,13 @@ local _M = {}
 _M.global_mapping = function()
 	-- vim.keymap.set('n', '<C-n>', ':bnext<CR>')
 	-- vim.keymap.set('n', '<C-p>', ':bnext<CR>')
+	local mfn = require('my.fn')
 
 	vim.keymap.set('n', '<C-w>t', ':split | terminal<CR>')
-	vim.keymap.set('n', "gf", require('my.fn').open_browser_or_buffer_under_cursor)
+	vim.keymap.set('n', "gf", mfn.open_browser_or_buffer_under_cursor)
 	vim.keymap.set('n', "<C-g><C-g>", '<Cmd>:echo v:statusmsg<CR>')
+	vim.keymap.set('n', "<Plug>(macro_edit)", mfn.macro_editor)
+	vim.keymap.set('n', '<leader>q', '<Plug>(macro_edit)', { desc = "Edit a macro", silent = true })
 end
 
 _M.filetype_mapping = function()
