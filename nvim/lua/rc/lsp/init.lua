68579bd0dd97d3d23bd717176d05d767f637aa37
tomato3713 <tomato3713@outlook.com>
2026-05-01 11:55:20 +0900

    built in 保管置き換え

--- nvim/lua/rc/lsp/init.lua
+++ nvim/lua/rc/lsp/init.lua
@@ -16,6 +16,15 @@ setup_keymap()
 
 vim.lsp.log.set_level("OFF")
 
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client and client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+
 vim.lsp.config('*', {
 	handlers = {
 		['textDocument/signatureHelp'] = function(err, result, ctx, config)
--- nvim/lua/rc/pum.lua
+++
@@ -1,9 +0,0 @@
-vim.fn['pum#set_option']({
-	item_orders = { 'kind', 'space', 'abbr', 'space', 'menu' },
-	max_width = 80,
-	max_height = 10,
-	border = 'single',
-	offset_cmdrow = 3,
-	padding = true,
-	preview = true,
-})
